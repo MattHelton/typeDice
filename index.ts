0c25d03437289ce7212b32e941ae2781360dc622
matthelton <mjoelhelton@me.com>
2022-04-08 11:53:40 -0400

    adds html element variables

---
+++ index.ts
@@ -0,0 +1,11 @@
+// create types for variables
+
+const ceeloButton: HTMLElement = document.getElementById('ceeloButton')
+const rollCeelo: HTMLElement = document.getElementById('ceelo')
+const threesButton: HTMLElement = document.getElementById('threesButton')
+const rollThrees: HTMLElement = document.getElementById('threes')
+const ThreesReset: HTMLElement = document.getElementById('threesReset')
+const ceeloReset: HTMLElement = document.getElementById('ceeloReset')
+const buttonDiv: HTMLElement = document.getElementById('buttonDiv')
+const choose: HTMLElement = document.getElementById('choose')
+
